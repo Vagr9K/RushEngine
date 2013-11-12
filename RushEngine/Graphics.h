08a8521054712a50d92cb571a7980e72dfc5c2e1
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-12 23:26:40 +0400

    Added some functions to Graphics class (including SDL_image Init() and Quit() ). Issues:  Issue #2 on BitBucket.

--- RushEngine/Graphics.h
+++ RushEngine/Graphics.h
@@ -1,7 +1,10 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include "Eventing.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <SDL_opengl.h>
 
 using namespace std;
 
@@ -9,9 +12,11 @@ class Graphics : public Eventing
 {
 private:
 	int Width, Height, LayerNumber;
-	char* Title;
+	const char* Title;
 	SDL_Window* main = NULL;
 	SDL_Renderer *Render = NULL;
+	vector<SDL_Surface> PreLoadedSurf;
+	vector<string> PreLoadedPaths;
 public:
 	Graphics(int Width, int Height, int LayerNumber,string Title)
 	{
@@ -41,6 +46,16 @@ public:
 			return false;
 		}
 
+		int IMGFlags = IMG_INIT_PNG;
+		int InitStat = IMG_Init(IMGFlags);
+		if (InitStat&IMGFlags != IMGFlags)
+		{
+			OnGraphicsError(IMG_GetError());
+			return false;
+		}
+
+
+
 		return true;
 	}
 
@@ -48,11 +63,56 @@ public:
 	{
 		SDL_DestroyRenderer(Render);
 		SDL_DestroyWindow(main);
+		IMG_Quit();
 		SDL_Quit();
 		return true;
 	}
 
+	inline SDL_Surface IMGLoad(string Path)
+	{
+		SDL_Surface* Temp1 = IMG_Load(Path.c_str());
+		SDL_Surface Temp = *Temp1;
+		delete Temp1;
+		return Temp;
+	}
+	void PreLoader(string PathToIMG)
+	{
+		PreLoadedPaths.push_back(PathToIMG);
+		PreLoadedSurf.push_back(IMGLoad(PathToIMG));
+	}
 
+	SDL_Surface* AngleTransform(SDL_Surface* Source, float Angle)
+	{
+		//Transformation goes here!
+		//OpenGL problems start here!
+	}
+
+
+
+	void Loader(int x, int y, int h, int w, string Path)
+	{
+		SDL_Surface Temp;
+		bool SurfPresent = false;
+		for (int i = 0; i < PreLoadedPaths.size(); i++)
+		{
+			if (PreLoadedPaths[i] == Path)
+			{
+				Temp = PreLoadedSurf[i];
+				break;
+			}
+		}
+		if (SurfPresent==false)
+
+		{
+			PreLoadedSurf.push_back(IMGLoad(Path));
+			PreLoadedPaths.push_back(Path);
+			Temp = PreLoadedSurf.back();
+		}
+
+		//Add more code
+
+
+	}
 
 
 };
