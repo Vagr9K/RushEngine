b1216166b4b54457e2c3ace75fd6c6711b5109cb
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-16 19:55:19 +0400

    Remake of eventing system. Function Logger() moved to Eventing class.

--- RushEngine/Eventing.h
+++ RushEngine/Eventing.h
@@ -1,32 +1,56 @@
-#include "Logger.h"
-#include <iostream>
+
+#include <fstream>
+#include <ios>
+#include <cstdlib>
+#include <ctime>
+#include <assert.h>
 using namespace std;
 
 
-class Eventing
+
+
+
+class GEEventing
 {
+
 public:
-	Eventing();
-	~Eventing();
-	void OnGraphicsError(string Error);
+	void LogError(string Type, string Error)
+	{
+
+		time_t TmNow = time(0);
+		struct tm TimeNow;
+		localtime_s(&TimeNow, &TmNow);
+		ofstream Log("logs.txt", std::ios_base::app | std::ios_base::out);;
 
 
+		Log << "\n";
+		Log << "\n";
+
+		Log << TimeNow.tm_mday;
+		Log << "-" + (TimeNow.tm_mon + 1);
+		Log << "-" + (TimeNow.tm_year + 1990);
+		Log << "-" + TimeNow.tm_hour;
+		Log << "-" + TimeNow.tm_min;
+
+		Log << "\n";
+		Log << "Error Type : ";
+		Log << Type.c_str();
+		Log << "\n";
+		Log << Error.c_str();
+		Log << "\n";
+		Log << "\n";
+		Log.close();
 
 
-private:
+		assert(("Error type: " + Type + "/n " + Error, true));
+
+
+	}
+	void OnGraphicsError(string Error)
+	{
+		LogError("Graphics Error", Error);
+	}
 
 };
 
-Eventing::Eventing()
-{
 
-}
-
-Eventing::~Eventing()
-{
-}
-
-void Eventing::OnGraphicsError(string Error)
-{
-	Logger::Log("Graphics", Error);
-}
