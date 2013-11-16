7f120992cc0b3581ad9d9f223c05f3e0f363971f
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-16 19:58:04 +0400

    Changes and improvements to Graphics class.

--- RushEngine/Graphics.h
+++ RushEngine/Graphics.h
@@ -1,56 +1,81 @@
+
+
 #include <SDL.h>
 #include <SDL_image.h>
-#include "Eventing.h"
-#include <iostream>
-#include <string>
-#include <vector>
 #include <SDL_opengl.h>
+
+#include "Eventing.h"
+
+
+#include <iostream>
+#include <vector>
+
 
 using namespace std;
 
-class Graphics : public Eventing
+class Graphics
 {
 private:
 	int Width, Height, LayerNumber;
-	const char* Title;
+	char Title;
+	bool IsReady = false; 
 	SDL_Window* main = NULL;
 	SDL_Renderer *Render = NULL;
-	vector<SDL_Surface> PreLoadedSurf;
+	vector<SDL_Surface*> PreLoadedSurf;
 	vector<string> PreLoadedPaths;
+	GEEventing* EvSys;
+	SDL_Surface* TEMP;
 public:
-	Graphics(int Width, int Height, int LayerNumber,string Title)
+	Graphics(){}
+	Graphics(int Width, int Height, int LayerNumber,string Title, GEEventing* EvVar)
 	{
 		this->Width = Width;
 		this->Height = Height;
 		this->LayerNumber = LayerNumber;
-		this->Title = Title.c_str();
+		this->Title = *(Title.c_str());
+		this->IsReady = true;
+		this->EvSys = EvVar;
+	}
+	void Init(int Width, int Height, int LayerNumber, string Title, GEEventing* EvVar)
+	{
+		this->Width = Width;
+		this->Height = Height;
+		this->LayerNumber = LayerNumber;
+		this->Title = *(Title.c_str());
+		this->EvSys = EvVar;
+		this->IsReady = true;
 	}
 	
 	bool Start()
 	{
+		if (IsReady==false)
+		{
+			return false;
+		} 
+	
 		if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		{
-			OnGraphicsError(SDL_GetError());
+			EvSys->OnGraphicsError(SDL_GetError());
 			return false;
 		}
-		main = SDL_CreateWindow(Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Width, Height, SDL_WINDOW_SHOWN);
+		main = SDL_CreateWindow(&Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Width, Height, SDL_WINDOW_SHOWN);
 		if (main==NULL)
 		{
-			OnGraphicsError(SDL_GetError());
+			EvSys->OnGraphicsError(SDL_GetError());
 			return false;
 		}
 		Render = SDL_CreateRenderer(main, -1, SDL_RENDERER_ACCELERATED);
 		if (Render==NULL)
 		{
-			OnGraphicsError(SDL_GetError());
+			EvSys->OnGraphicsError(SDL_GetError());
 			return false;
 		}
 
 		int IMGFlags = IMG_INIT_PNG;
 		int InitStat = IMG_Init(IMGFlags);
-		if (InitStat&IMGFlags != IMGFlags)
+		if (InitStat != IMGFlags)
 		{
-			OnGraphicsError(IMG_GetError());
+			EvSys->OnGraphicsError(IMG_GetError());
 			return false;
 		}
 
@@ -67,13 +92,40 @@ public:
 		SDL_Quit();
 		return true;
 	}
+	void DelayGraphics(int Milliseconds)
+	{
+		SDL_Delay(Milliseconds);
+	}
 
-	inline SDL_Surface IMGLoad(string Path)
+	void BlackInit()
+	{
+		
+		SDL_Rect rectData;
+		rectData.h = Height;
+		rectData.w = Width;
+		rectData.x = 0;
+		rectData.y = 0;
+		SDL_Surface* TEMP = Loader("images/BlackInit.png");
+		SDL_Texture* Texture = SDL_CreateTextureFromSurface(Render, TEMP);
+		SDL_RenderClear(Render);
+		SDL_RenderCopy(Render, Texture, NULL, NULL);
+		SDL_RenderPresent(Render);
+		SDL_FreeSurface(TEMP);
+		SDL_DestroyTexture(Texture);
+		
+	}
+
+
+	inline SDL_Surface* IMGLoad(string Path)
 	{
 		SDL_Surface* Temp1 = IMG_Load(Path.c_str());
-		SDL_Surface Temp = *Temp1;
-		delete Temp1;
-		return Temp;
+		if (Temp1==NULL)
+		{
+			EvSys->OnGraphicsError(IMG_GetError());
+		}
+		
+		
+		return Temp1;
 	}
 	void PreLoader(string PathToIMG)
 	{
@@ -89,11 +141,11 @@ public:
 
 
 
-	void Loader(int x, int y, int h, int w, string Path)
+	SDL_Surface* Loader(string Path)
 	{
-		SDL_Surface Temp;
+		SDL_Surface* Temp = NULL;
 		bool SurfPresent = false;
-		for (int i = 0; i < PreLoadedPaths.size(); i++)
+		for (unsigned int i = 0; i < PreLoadedPaths.size(); i++)
 		{
 			if (PreLoadedPaths[i] == Path)
 			{
@@ -108,10 +160,7 @@ public:
 			PreLoadedPaths.push_back(Path);
 			Temp = PreLoadedSurf.back();
 		}
-
-		//Add more code
-
-
+		return Temp;
 	}
 
 
