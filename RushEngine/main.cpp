9c383cf680ae4a9cdfeb85c7ee7fec78ee9499ea
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-16 19:59:35 +0400

    Added some code for game engine pre-alfa tests.

--- RushEngine/main.cpp
+++ RushEngine/main.cpp
@@ -1,8 +1,28 @@
-#include <SDL.h>
-#include "Logger.h"
+#include "GameEngine.h"
+
+
+
 
 int main(int argc, char** argv)
 {
+	GameEngine mainEngine("Name");
+	 bool a = mainEngine.StartGraphics(450, 500, 4, "Dead Zone");
+	 
+	 bool Die = false;
+	 while (true)
+	 {
+		 /*
+		 if(SDL_PollEvent(&e) != 0) 
+		 if (e.type==SDL_QUIT)
+		 {
+			 Die = true;
+			
+		 }
+		 */
+		 mainEngine.BlackInitGraphics();
+		 mainEngine.getEventSystem().LogError("FPS", "FPS");
+	 }
+	mainEngine.StopGraphics();
 	
 	return 0;
 }
