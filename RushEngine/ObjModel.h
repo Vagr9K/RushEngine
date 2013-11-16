88290266601ddf5fab0c7d49fc9e9c56c3b6e5ca
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-16 20:00:13 +0400

    Added IMG and TXT classes for further Data class development.

--- RushEngine/ObjModel.h
+++ RushEngine/ObjModel.h
@@ -4,7 +4,7 @@ using namespace std;
 #include "Box2D.h"
 
 
-class MainObj
+class MainObj 
 {
 public:
 	
@@ -16,9 +16,46 @@ public:
 class MinObj 
 {
 public:
-	
-	
+
 
 
 
 };
+
+struct IMG
+{
+
+public:
+	int x, y, w, h;
+	double Angle;
+	string Source;
+	IMG(int x, int y, int w, int h, double Angle, string Source)
+	{
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		this->Angle = Angle;
+		this->Source = Source;
+	}
+	
+};
+
+struct TXT
+{
+public:
+	int x, y, w, h;
+	double Angle;
+	string Content;
+	TXT(int x, int y, int w, int h, double Angle, string Content)
+	{
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		this->Angle = Angle;
+		this->Content = Content;
+	}
+	
+};
+
