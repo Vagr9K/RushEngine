ce7c677648efb6d661d57fe594228de9033c2dee
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-18 22:35:08 +0400

    Modified main.cpp for easier tests.

--- RushEngine/main.cpp
+++ RushEngine/main.cpp
@@ -7,18 +7,18 @@ int main(int argc, char** argv)
 {
 	GameEngine mainEngine("Name");
 	 bool a = mainEngine.StartGraphics(450, 500, 4, "Dead Zone");
-	 
+	 SDL_Event e;
 	 bool Die = false;
-	 while (true)
+	 while (Die == false)
 	 {
-		 /*
+		 
 		 if(SDL_PollEvent(&e) != 0) 
 		 if (e.type==SDL_QUIT)
 		 {
 			 Die = true;
 			
 		 }
-		 */
+		 
 		 mainEngine.BlackInitGraphics();
 		 mainEngine.getEventSystem().LogError("FPS", "FPS");
 	 }
