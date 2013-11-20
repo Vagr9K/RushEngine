0bf801c6ae4b44dffe89d2013f883bd04223a2f6
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-21 00:44:43 +0400

    Made changes for RAM usage and OPS tests.

--- RushEngine/main.cpp
+++ RushEngine/main.cpp
@@ -7,7 +7,19 @@ int main(int argc, char** argv)
 {
 	GameEngine mainEngine("Name");
 	 bool a = mainEngine.StartGraphics(450, 500, 4, "Dead Zone");
+	 mainEngine.InitObjects(12, 12, 12, 12);
+	 mainEngine.InitPhysics();
+	 LayerElement* b;
+	 for (int i = 0; i < 100000; i++)
+	 {
+		 b = new LayerElement;
+		 b->Image = new IMG(12, 12, 12, 12, 12.12, "Source");
+		 b->Text = new TXT(1, 2, 3, 4, 5, "String");
+		 mainEngine.Objects->GrLayers[0].push_back(b);
+		 b = NULL;
+	 }
 	 SDL_Event e;
+	 OPSTester OPS;
 	 bool Die = false;
 	 while (Die == false)
 	 {
@@ -20,7 +32,7 @@ int main(int argc, char** argv)
 		 }
 		 
 		 mainEngine.BlackInitGraphics();
-		 mainEngine.getEventSystem().LogError("FPS", "FPS");
+		 OPS.TestOPS(10);
 	 }
 	mainEngine.StopGraphics();
 	
