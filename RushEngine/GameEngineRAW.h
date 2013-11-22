aa042bf72d10a2a411a3051d81d555aab1fc9156
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-22 18:23:30 +0400

    Renamed GameEngine.h to GameEngineRAW.h for loaw-level accesses.

---
+++ RushEngine/GameEngineRAW.h
@@ -0,0 +1,105 @@
+#include "Graphics.h"
+#include "Objects.h"
+#include "Physics.h"
+
+
+class GameEngine
+{
+private:
+	string GameName, Title;
+	int h, w, LayerNumber;
+	
+	string Errors;
+	GEEventing EvSys;
+
+public:
+	PhysicsEngine* Physics;
+	ObjectsEngine* Objects;
+	GraphicsEngine* Graphics;
+
+	void InitPhysics()
+	{
+		Physics = new PhysicsEngine;
+	}
+
+	void InitObjects(int InterfaceLayerCount, int SpecialLayerCount, int WorldCount, int BackGroundLayerCount)
+	{
+		Objects = new ObjectsEngine(InterfaceLayerCount, SpecialLayerCount, WorldCount, BackGroundLayerCount);
+	}
+
+	GameEngine(string GameName)
+	{ 
+		this->GameName = GameName;
+	}
+	bool StartGraphics(int Width, int Height, int LayerNumber, string Title)
+	{
+		this->h = Height;
+		this->w = Width;
+		this->LayerNumber = LayerNumber;
+		this->Title = Title;
+		Graphics = new GraphicsEngine;
+		Graphics->Init(Width, Height, LayerNumber, Title, &EvSys);
+		return Graphics->Start();
+
+	}
+
+
+
+	bool StopGraphics()
+	{
+		return Graphics->Stop();
+	}
+
+	void DelayGraphics(int Milliseconds)
+	{
+		Graphics->DelayGraphics(Milliseconds);
+	}
+	void BlackInitGraphics()
+	{
+		Graphics->BlackInit();
+	}
+	GEEventing getEventSystem()
+	{
+		return EvSys;
+	}
+
+};
+
+class OPSTester
+{
+	time_t startFPS, endFPS;
+	bool FPSStarted = false;
+	double FPSTestTime;
+	int Frames = 0;
+	GEEventing event;
+public:
+	void TestOPS(double TestTime)
+	{
+		if (FPSStarted == false)
+		{
+
+			time(&startFPS);
+			FPSStarted = true;
+			FPSTestTime = TestTime;
+			Frames = 1;
+		}
+		else
+		{
+
+			time(&endFPS);
+			double DiffTime = difftime(endFPS, startFPS);
+			if (DiffTime >= FPSTestTime)
+			{
+				double FPSActual = Frames/DiffTime;
+				std::ostringstream ToString;
+				ToString << FPSActual;
+				event.OPSLog(ToString.str());
+				FPSStarted = false;
+			}
+			else
+			{
+				Frames++;
+			}
+		}
+	}
+};
