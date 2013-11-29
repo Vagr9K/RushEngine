c315e304b4f18c8365a2038772842bf6b6623b95
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-11-30 01:34:40 +0400

    Added enums Mode, Style, struct TextFont. Now Graphics manager supports rendering text and OptimalObjectCount settings.

--- RushEngine/Graphics.h
+++ RushEngine/Graphics.h
@@ -5,6 +5,7 @@
 #include <sstream>
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <SDL_opengl.h>
 
@@ -12,6 +13,46 @@
 
 
 using namespace std;
+enum Mode
+{
+	SOLID,
+	SHADED,
+	BLENDED,
+
+};
+enum Style
+
+{
+	NORMAL,
+	BOLD,
+	ITALIC,
+	STRIKETHROUGH,
+	UNDERLINE
+
+};
+class GameEngine;
+
+struct TextFont
+{
+
+
+	string FontPath;
+	int PointSize=12;
+	int FontOutline =0;
+	Style FontStyle = NORMAL;
+	int Index = 0;
+	bool FontKerning = false;
+
+
+public:
+
+	TextFont(string FontPath)
+	{
+		this->FontPath = FontPath;
+
+	}
+};
+
 
 class GraphicsManager
 {
@@ -22,6 +63,17 @@ class GraphicsManager
 	vector<string>* PreLoadedPathsGPU=NULL;
 	EventingEngine* EventEngine=NULL;
 	SDL_Renderer* Renderer=NULL;
+	int OptimalObjectCount=0;
+
+	vector<TTF_Font*> LoadedFonts;
+	vector<string> LoadedFontsPaths;
+	vector<SDL_Surface*> LoadedTextsCPU;
+	vector<string> LoadedTextArgsCPU;
+	vector<string> LoadedTextArgsGPU;
+	vector<SDL_Texture*> LoadedTextsGPU;
+	vector<int> TimeFromLastUseCPU;
+	vector<int> TimeFromLastUseGPU;
+	int MaximumTimeFromLastUse = 40;
 
 
 public:
@@ -42,7 +94,25 @@ public:
 		this->Renderer = Renderer;
 		InitPreloaders();
 	}
+	GraphicsManager(EventingEngine *EventEngine, SDL_Renderer* Renderer, int OptimalObjectCount)
+	{
+		this->EventEngine = EventEngine;
+		this->Renderer = Renderer;
+		this->OptimalObjectCount = OptimalObjectCount;
+		InitPreloaders();
+	}
 
+	void SetOptimalObjectCount(int OptimalObjectCount)
+	{
+		this->OptimalObjectCount = OptimalObjectCount;
+	}
+
+	int GetOptimalObjectCount()
+	{
+		return this->OptimalObjectCount;
+	}
+
+	
 	void InitPreloaders()
 	{
 		if (PreLoadedPathsCPU !=NULL)
@@ -61,11 +131,241 @@ public:
 		{
 			delete[] PreLoadedTextGPU;
 		}
-		this->PreLoadedPathsCPU = new vector<string>;
-		this->PreLoadedPathsGPU = new vector<string>;
-		this->PreLoadedSurfCPU = new vector <SDL_Surface*>;
-		this->PreLoadedTextGPU = new vector<SDL_Texture*>;
+		this->PreLoadedPathsCPU = new vector<string>(OptimalObjectCount);
+		this->PreLoadedPathsGPU = new vector<string>(OptimalObjectCount);
+		this->PreLoadedSurfCPU = new vector <SDL_Surface*>(OptimalObjectCount);
+		this->PreLoadedTextGPU = new vector<SDL_Texture*>(OptimalObjectCount);
 	}
+	
+	TTF_Font* LoadFont(string FontPath, int PointSize, int Index = 0)
+	{
+		TTF_Font* Font = NULL;
+		if (Index == 0)
+		{
+			Font = TTF_OpenFont(FontPath.c_str(), PointSize);
+			if (Font == NULL)
+			{
+				EventEngine->GraphicsError(TTF_GetError());
+
+			}
+			return Font;
+		}
+		else
+		{
+			Font = TTF_OpenFontIndex(FontPath.c_str(), PointSize, Index);
+			if (Font == NULL)
+			{
+				EventEngine->GraphicsError(TTF_GetError());
+
+			}
+			return Font;
+		}
+		return Font;
+
+	}
+
+	TTF_Font* GetFont(string FontPath, int PointSize, int FontOutline, Style FontStyle, int Index = 0, bool FontKerning = false)
+	{
+		string Path = FontPath + to_string(PointSize) + to_string(Index);
+		for (unsigned int i = 0; i < LoadedFontsPaths.size(); i++)
+		{
+			if (LoadedFontsPaths.at(i) == Path)
+			{
+				return LoadedFonts.at(i);
+			}
+		}
+
+
+
+
+		TTF_Font* Font = LoadFont(FontPath, PointSize, Index);
+		if (FontOutline != 0)
+		{
+			TTF_SetFontOutline(Font, FontOutline);
+
+		}
+		if (FontKerning == true)
+		{
+			TTF_SetFontKerning(Font, 1);
+		}
+		switch (FontStyle)
+		{
+		case NORMAL:
+			TTF_SetFontStyle(Font, TTF_STYLE_NORMAL);
+			break;
+		case BOLD:
+			TTF_SetFontStyle(Font, TTF_STYLE_BOLD);
+			break;
+		case ITALIC:
+			TTF_SetFontStyle(Font, TTF_STYLE_ITALIC);
+			break;
+		case STRIKETHROUGH:
+			TTF_SetFontStyle(Font, TTF_STYLE_STRIKETHROUGH);
+			break;
+		case UNDERLINE:
+			TTF_SetFontStyle(Font, TTF_STYLE_UNDERLINE);
+			break;
+		default:
+			break;
+		}
+		LoadedFonts.push_back(Font);
+		LoadedFontsPaths.push_back(Path);
+		return Font;
+
+	}
+
+
+
+	SDL_Surface* GetTextImageCPU(TextFont* Font,string Text, Mode DrawMode, SDL_Color Foregroung, SDL_Color Background)
+	{
+		SDL_Surface* Surf = NULL;
+		string args = to_string(Font->FontKerning) + to_string(Font->FontOutline)
+			+ Font->FontPath + to_string(Font->FontStyle) + to_string(Font->Index)
+			+ to_string(Font->PointSize) + Text;
+		for (unsigned int i = 0; i < TimeFromLastUseCPU.size(); i++)
+		{
+			if (LoadedTextArgsCPU.at(i) == args)
+			{
+				
+				TimeFromLastUseCPU.at(i) = 0;
+				return LoadedTextsCPU.at(i);
+				Surf = LoadedTextsCPU.at(i);
+				break;
+			}
+			else
+			{
+				if (TimeFromLastUseCPU.at(i)>=MaximumTimeFromLastUse)
+				{
+					LoadedTextsCPU.erase(LoadedTextsCPU.begin() + i);
+					TimeFromLastUseCPU.erase(TimeFromLastUseCPU.begin() + i);
+					LoadedTextArgsCPU.erase(LoadedTextArgsCPU.begin() + i);
+					i--;
+
+				} 
+				else
+				{
+					TimeFromLastUseCPU.at(i)++;
+				}
+			}
+		}
+		TTF_Font* TTFFONT = GetFont(Font->FontPath, Font->PointSize, Font->FontOutline, Font->FontStyle, Font->Index, Font->FontKerning);
+		switch (DrawMode)
+		{
+		case SOLID:
+			Surf = GetSurfaceSolid(TTFFONT, Text, Foregroung);
+			break;
+		case SHADED:
+			Surf = GetSurfaceShaded(TTFFONT, Text, Foregroung, Background);
+			break;
+		case BLENDED:
+			Surf = GetSurfaceBlended(TTFFONT, Text, Foregroung);
+			break;
+		default:
+			break;
+		}
+		LoadedTextsCPU.push_back(Surf);
+		TimeFromLastUseCPU.push_back(0);
+		LoadedTextArgsCPU.push_back(args);
+
+
+
+		return Surf;
+	}
+
+
+	SDL_Texture* GetTextImageGPU(TextFont* Font, string Text, Mode DrawMode, SDL_Color Foregroung, SDL_Color Background)
+	{
+		SDL_Texture* Texture = NULL;
+		string args = to_string(Font->FontKerning) + to_string(Font->FontOutline)
+			+ Font->FontPath + to_string(Font->FontStyle) + to_string(Font->Index)
+			+ to_string(Font->PointSize) + Text;
+		for (unsigned int i = 0; i < TimeFromLastUseGPU.size(); i++)
+		{
+			if (LoadedTextArgsGPU.at(i) == args)
+			{
+
+				TimeFromLastUseGPU.at(i) = 0;
+				return LoadedTextsGPU.at(i);
+				Texture = LoadedTextsGPU.at(i);
+				break;
+			}
+			else
+			{
+				if (TimeFromLastUseGPU.at(i) >= MaximumTimeFromLastUse)
+				{
+					LoadedTextsGPU.erase(LoadedTextsGPU.begin() + i);
+					TimeFromLastUseGPU.erase(TimeFromLastUseGPU.begin() + i);
+					LoadedTextArgsGPU.erase(LoadedTextArgsGPU.begin() + i);
+					i--;
+
+				}
+				else
+				{
+					TimeFromLastUseGPU.at(i)++;
+				}
+			}
+		}
+		TTF_Font* TTFFONT = GetFont(Font->FontPath, Font->PointSize, Font->FontOutline, Font->FontStyle, Font->Index, Font->FontKerning);
+		switch (DrawMode)
+		{
+		case SOLID:
+			Texture = SDL_CreateTextureFromSurface(Renderer,GetSurfaceSolid(TTFFONT, Text, Foregroung));
+			break;
+		case SHADED:
+			Texture = SDL_CreateTextureFromSurface(Renderer,GetSurfaceShaded(TTFFONT, Text, Foregroung, Background));
+			break;
+		case BLENDED:
+			Texture = SDL_CreateTextureFromSurface(Renderer, GetSurfaceBlended(TTFFONT, Text, Foregroung));
+			break;
+		default:
+			break;
+		}
+		LoadedTextsGPU.push_back(Texture);
+		TimeFromLastUseGPU.push_back(0);
+		LoadedTextArgsGPU.push_back(args);
+
+
+
+		return Texture;
+	}
+
+
+
+	SDL_Surface* GetSurfaceSolid(TTF_Font* Font, string Text, SDL_Color Foreground)
+	{
+		SDL_Surface* Surf = NULL;
+		Surf = TTF_RenderText_Solid(Font, Text.c_str(), Foreground);
+		if (Surf == NULL)
+		{
+			EventEngine->GraphicsError(TTF_GetError());
+		}
+		return Surf;
+	}
+
+	SDL_Surface* GetSurfaceShaded(TTF_Font* Font, string Text, SDL_Color Foreground, SDL_Color Background)
+	{
+		SDL_Surface* Surf = NULL;
+		Surf = TTF_RenderText_Shaded(Font, Text.c_str(), Foreground, Background);
+		if (Surf == NULL)
+		{
+			EventEngine->GraphicsError(TTF_GetError());
+		}
+		return Surf;
+	}
+
+	SDL_Surface* GetSurfaceBlended(TTF_Font* Font, string Text, SDL_Color Foreground)
+	{
+		SDL_Surface* Surf = NULL;
+		Surf = TTF_RenderText_Blended(Font, Text.c_str(), Foreground);
+		if (Surf == NULL)
+		{
+			EventEngine->GraphicsError(TTF_GetError());
+		}
+		return Surf;
+	}
+
+	
+
 	
 	SDL_Surface* LoaderCPU(string Path)
 	{
@@ -241,7 +541,30 @@ public:
 		SDL_BlitSurface(ManagerGR->LoaderCPU(Source), NULL, WinSurf, &CopyTo);
 
 	}
+	void AddToBuffer(int X, int Y, int W, int H, string Text, TextFont *Font, Mode DrawMode, SDL_Color Foreground)
+	{
+		SDL_Color Background;
+		Background.a = 0;
+		Background.b = 0;
+		Background.g = 0;
+		Background.r = 0;
+		CopyTo.h = H;
+		CopyTo.w = W;
+		CopyTo.x = X;
+		CopyTo.y = Y;
+		SDL_BlitSurface(ManagerGR->GetTextImageCPU(Font, Text, DrawMode, Foreground, Background), NULL, WinSurf, &CopyTo);
 
+	}
+	void AddToBuffer(int X, int Y, int W, int H, string Text, TextFont *Font, Mode DrawMode, SDL_Color Foreground, SDL_Color Background)
+	{
+
+		CopyTo.h = H;
+		CopyTo.w = W;
+		CopyTo.x = X;
+		CopyTo.y = Y;
+		SDL_BlitSurface(ManagerGR->GetTextImageCPU(Font, Text, DrawMode, Foreground, Background), NULL, WinSurf, &CopyTo);
+
+	}
 	void PushBuffer()
 	{
 		SDL_UpdateWindowSurface(Window);
@@ -305,6 +628,30 @@ public:
 		SDL_RenderCopy(Render, ManagerGR->LoaderGPU(Source), &CopyFrom, &CopyTo);
 
 	}
+	void AddToBuffer(int X, int Y, int W, int H, string Text, TextFont *Font, Mode DrawMode, SDL_Color Foreground, SDL_Color Background)
+	{
+
+		CopyTo.h = H;
+		CopyTo.w = W;
+		CopyTo.x = X;
+		CopyTo.y = Y;
+		SDL_RenderCopy(Render, ManagerGR->GetTextImageGPU(Font, Text, DrawMode, Foreground, Background), NULL, &CopyTo);
+
+	}
+	void AddToBuffer(int X, int Y, int W, int H, string Text, TextFont *Font, Mode DrawMode, SDL_Color Foreground)
+	{
+		SDL_Color Background;
+		Background.a = 0;
+		Background.b = 0;
+		Background.g = 0;
+		Background.r = 0;
+		CopyTo.h = H;
+		CopyTo.w = W;
+		CopyTo.x = X;
+		CopyTo.y = Y;
+		SDL_RenderCopy(Render, ManagerGR->GetTextImageGPU(Font, Text, DrawMode, Foreground, Background), NULL, &CopyTo);
+
+	}
 	void AddToBuffer(int X, int Y, int W, int H, string Source)
 	{
 
@@ -349,6 +696,7 @@ private:
 	SDL_Renderer *Renderer = NULL;
 	EventingEngine* EventEngine = NULL;
 	GraphicsManager* ManagerGR = NULL;
+
 	
 	bool CPUInited = false;
 
@@ -415,7 +763,11 @@ public:
 			EventEngine->GraphicsError(SDL_GetError());
 
 		}
-		
+		if (TTF_Init() < 0)
+		{
+			EventEngine->GraphicsError(TTF_GetError());
+		}
+
 		int IMGFlags = IMG_INIT_PNG;
 		int InitStat = IMG_Init(IMGFlags);
 		if (InitStat != IMGFlags)
@@ -423,10 +775,11 @@ public:
 			EventEngine->GraphicsError(IMG_GetError());
 			return false;
 		}
+		
 
 
 
-		//this->ManagerGR = new GraphicsManager(EventEngine, Render, &PreLoadedSurfCPU, &PreLoadedTextGPU,&PreLoadedPathsCPU, &PreLoadedPathsGPU);
+		
 		this->ManagerGR = new GraphicsManager(EventEngine, Renderer);
 		IsStarted = true;
 		return true;
@@ -464,6 +817,7 @@ public:
 		
 		SDL_DestroyWindow(mainWindow);
 		IMG_Quit();
+		TTF_Quit();
 		SDL_Quit();
 
 		if (CPUInited==true)
