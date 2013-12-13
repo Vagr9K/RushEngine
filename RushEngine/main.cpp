2d0cdc3afaae2b32a20fdd1732fda196c202b46a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2013-12-13 15:09:08 +0400

    Added latest test to project.

--- RushEngine/main.cpp
+++ RushEngine/main.cpp
@@ -1,139 +1,182 @@
 #include "GameEngine.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+using namespace std;
 
 #define WHEIGHT 900
-#define WWIDTH 680
+#define WWIDTH 1000
 
 
+#define  BallCount  40
+#define  WallCount  4
 
+#define  K 100
+
+GameEngine mainEngine;
+
+vector<Object*> Balls(BallCount);
+vector<Object*> Walls(WallCount);
+b2Vec2 Gravity(0.0f, -10.0f);
+b2World World(Gravity);
+b2CircleShape* STDShape; 
+b2PolygonShape* W1;
+b2PolygonShape* W2;
+
+int CamX = 0;
+int CamY = 0;
+
+SDL_Event* Ev;
+
+int RndGen(int Max)
+{
+	int Ret = rand() % Max;
+	return Ret;
+}
+
+
+void Init()
+{
+	
+	mainEngine.InitPhysics();
+	mainEngine.InitGraphics(WWIDTH, WHEIGHT, 1, "Test #002.");
+	mainEngine.Graphics->Start();
+	mainEngine.Graphics->InitGL();
+	mainEngine.InitObjects(0,0,1,0);
+	mainEngine.Physics->AddWorld(&World);
+	STDShape = new b2CircleShape;
+	STDShape->m_p.Set(0.0f, 0.0f);
+	STDShape->m_radius = 0.1f;
+	W1 = new b2PolygonShape;
+	W1->SetAsBox(10.f, 5.f);
+	W2 = new b2PolygonShape;
+	W2->SetAsBox(5.f, 10.f);
+	float Rnd1 = 0;
+	float Rnd2 = 0;
+	for (int i = 0; i < BallCount; i++)
+	{
+		Rnd1 = RndGen(10);
+		Rnd2 = RndGen(10);
+		Balls.at(i) = new Object(&mainEngine, 0, 0);
+		Balls.at(i)->BodyDefinition = new b2BodyDef;
+		Balls.at(i)->BodyDefinition->type = b2_dynamicBody;
+		Balls.at(i)->BodyDefinition->position.Set(Rnd1, Rnd2);
+		Balls.at(i)->CreateBody();
+		Balls.at(i)->FixtureDefinition = new b2FixtureDef;
+		Balls.at(i)->FixtureDefinition->restitution = 1.1f;
+		Balls.at(i)->FixtureDefinition->shape = STDShape;
+		Balls.at(i)->CreateFixture();
+
+
+	}
+
+	for (int j = 0; j < WallCount;j++)
+	{
+		Walls.at(j) = new Object(&mainEngine, 0, 0);
+		Walls.at(j)->BodyDefinition = new b2BodyDef;
+		Walls.at(j)->BodyDefinition->type = b2_kinematicBody;
+
+
+		Walls.at(j)->FixtureDefinition = new b2FixtureDef;
+		Walls.at(j)->FixtureDefinition->restitution = 1.0f;
+
+	}
+
+	Walls.at(0)->BodyDefinition->position.Set(5.f, -2.5f);
+	Walls.at(0)->CreateBody();
+	Walls.at(0)->FixtureDefinition->shape = W1;
+	Walls.at(0)->CreateFixture();
+	Walls.at(1)->BodyDefinition->position.Set(-2.5f, 5.f);
+	Walls.at(1)->CreateBody();
+	Walls.at(1)->FixtureDefinition->shape = W2;
+	Walls.at(1)->CreateFixture();
+	Walls.at(2)->BodyDefinition->position.Set(5.f, 12.5f);
+	Walls.at(2)->CreateBody();
+	Walls.at(2)->FixtureDefinition->shape = W1;
+	Walls.at(2)->CreateFixture();
+	Walls.at(3)->BodyDefinition->position.Set(12.5f, 5.f);
+	Walls.at(3)->CreateBody();
+	Walls.at(3)->FixtureDefinition->shape = W2;
+	Walls.at(3)->CreateFixture();
+
+	Ev = new SDL_Event;
+
+}
+
+
+void RenderPhysics()
+{
+	float Step = 1.f / 70.f;
+	World.Step(Step, 10, 3);
+
+
+}
+
+void RenderGraphics()
+{
+	float Angle = 0;
+	b2Vec2 Pos;
+	mainEngine.Graphics->DrawerGL->StartBuffer();
+	for (int i = 0; i < BallCount;i++)
+	{
+		Pos = Balls.at(i)->Body->GetPosition();
+		Angle = Balls.at(i)->Body->GetAngle();
+		mainEngine.Graphics->DrawerGL->AddToBuffer(Pos.x*K, WHEIGHT - Pos.y*K, 0.2*K, 0.2*K, "Ball.png", Angle);
+	}
+	
+		Pos = Walls.at(0)->Body->GetPosition();
+		mainEngine.Graphics->DrawerGL->AddToBuffer(Pos.x * K, WHEIGHT - Pos.y * K, 10 * K, 5 * K, "Wall.png");
+		Pos = Walls.at(1)->Body->GetPosition();
+		mainEngine.Graphics->DrawerGL->AddToBuffer(Pos.x * K, WHEIGHT - Pos.y * K, 5 * K, 10 * K, "Wall.png");
+		Pos = Walls.at(2)->Body->GetPosition();
+		mainEngine.Graphics->DrawerGL->AddToBuffer(Pos.x * K, WHEIGHT - Pos.y * K, 10 * K, 5 * K, "Wall.png");
+		Pos = Walls.at(3)->Body->GetPosition();
+		mainEngine.Graphics->DrawerGL->AddToBuffer(Pos.x * K, WHEIGHT - Pos.y * K, 5 * K, 10 * K, "Wall.png");
+	
+		mainEngine.Graphics->DrawerGL->PushBuffer();
+		
+}
+
+void RenderCamera()
+{
+	const Uint8* A = SDL_GetKeyboardState(NULL);
+	if (A[SDL_SCANCODE_RIGHT])
+	{
+		CamX += 40;
+	}
+	else if (A[SDL_SCANCODE_LEFT])
+	{
+		CamX -= 40;
+	}
+	else if (A[SDL_SCANCODE_UP])
+	{
+		CamY += 40;
+	}
+	else if (A[SDL_SCANCODE_DOWN])
+	{
+		CamY = CamY - 40;
+	}
+	mainEngine.Graphics->DrawerGL->SetViewPort(CamX, CamY, WHEIGHT, WWIDTH);
+
+}
 
 int main(int argc, char** argv)
 {
-	b2Vec2 Gravity(0,-10.0);
-	
-	b2World MainWorld(Gravity);
-	GameEngine* mainEngine = new GameEngine;
-	mainEngine->InitGraphics(WWIDTH, WHEIGHT, 2, "Very stupid physics test...");
-	mainEngine->Graphics->Start();
-	mainEngine->Graphics->InitGL();
-
-	mainEngine->InitObjects(0, 0, 1, 0);
-	mainEngine->InitPhysics();
-	mainEngine->Physics->AddWorld(&MainWorld);
-
-
-
-	
-	SDL_Event e;
-
-	
-	int StartTime = GetTickCount();
-	int Now = GetTickCount();
-	int Delta=0;
-	const int FPS = 60;
-	 bool Die = false;
-	 Object Ball(mainEngine, 0,0);
-	 Ball.BodyDefinition = new b2BodyDef;
-	 Ball.BodyDefinition->type = b2_dynamicBody;
-	 Ball.BodyDefinition->position.Set(5, 8);
-	 Ball.CreateBody();
-	 Ball.FixtureDefinition = new b2FixtureDef;
-	 Ball.FixtureDefinition->restitution = 1.0f;
-	 b2Vec2 Vert1[3]; 
-	 Vert1[0].Set(0, 0);
-	 Vert1[1].Set(1, 0);
-	 Vert1[2].Set(0.5, 1);
-	 b2PolygonShape Shape;
-	 Shape.Set(Vert1, 3);
-	 
-	 Ball.FixtureDefinition->shape =&Shape;
-	 Ball.CreateFixture();
-
-
-	 b2BodyDef groundBodyDef;
-	 groundBodyDef.position.Set(0.0f, -10.0f);
-	 b2Body* groundBody = MainWorld.CreateBody(&groundBodyDef);
-	 b2PolygonShape groundBox;
-	 groundBox.SetAsBox(50.0f, 10.0f);
-	 groundBody->CreateFixture(&groundBox, 0.0f);
-
-	 float32 Hz = 1.0f/30.0f;
-	 MainWorld.Step(Hz, 10, 8);
-
-	 b2Vec2 Pos1;
-	 b2Vec2 Pos2;
-	 TextFont FontofText("font.ttf");
-	 FontofText.PointSize = 50;
-
-	 SDL_Color Foreground;
-	 Foreground.a = 234;
-	 Foreground.b = 34;
-	 Foreground.g = 56;
-	 Foreground.r = 23;
-
-	 SDL_Color Back;
-	 Back.a = 23;
-	 Back.b = 234;
-	 Back.g = 23;
-	 Back.r = 245;
-	 string Text;
-	 int Angle1 = 0;
-	 int VX = 0;
-	 int VY = 0;
-	while (Die == false)
-	 {
-		Now = GetTickCount();
-		Delta = Now - StartTime;
-		if (Delta>=(Hz))
+	Init();
+	bool Close=false;
+	while (Close==false)
+	{
+		SDL_PollEvent(Ev);
+		if (Ev->type == SDL_QUIT)
 		{
-			StartTime = GetTickCount();
-			if (SDL_PollEvent(&e) != 0)
-			if (e.type == SDL_QUIT)
-			{
-				Die = true;
-
-			}
-			MainWorld.Step(Hz, 10, 8);
-			Pos1 = Ball.Body->GetPosition();
-			float32 Angle = Ball.Body->GetAngle();
-			Text = to_string(static_cast<long long>(Pos1.x * 100)) + " " + to_string(static_cast<long long>(Pos1.y * 100));
-			Angle1++;
-
-			const Uint8* A = SDL_GetKeyboardState(NULL);
-			if (A[SDL_SCANCODE_RIGHT])
-			{
-				VX+=40;
-			} 
-			else if (A[SDL_SCANCODE_LEFT])
-			{
-				VX-=40;
-			}
-			else if (A[SDL_SCANCODE_UP])
-			{
-				VY+=40;
-			}
-			else if (A[SDL_SCANCODE_DOWN])
-			{
-				VY=VY-40;
-			}
-			
-			mainEngine->Graphics->DrawerGL->StartBuffer();
-			
-			
-			mainEngine->Graphics->DrawerGL->AddToBuffer(Pos1.x*100, Pos1.y*100, 30, 30, "images/BlackInit.png", Angle);
-			mainEngine->Graphics->DrawerGL->AddToBuffer((Pos1.x*100+50),Pos1.y*100, 30, 30, "images/image.png", Angle1);
-			
-			mainEngine->Graphics->DrawerGL->SetViewPort(VX, VY, WHEIGHT, WWIDTH);
-			mainEngine->Graphics->DrawerGL->PushBuffer();
-			
-			
-
+			Close = true;
 		}
-		
-	 }
+		RenderPhysics();
 
-	 mainEngine->DeleteGraphics();
+		RenderGraphics();
+		RenderCamera();
+	}
+
 	
 	return 0;
 }
