86053f25380760d21ec40e7d3f9eb1182e5c22e2
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2014-01-14 20:00:38 +0400

    Added global SDL_Event for game engine.

--- RushEngine/Eventing/Eventing.h
+++ RushEngine/Eventing/Eventing.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "EventManagers.h"
+#include "SDL.h"
 using namespace std;
 
 
 
 class EventingEngine
 {
-
+public:
+	SDL_Event GlobalEvent;
 public:
 	SystemEventing SystemEvents;
 };
